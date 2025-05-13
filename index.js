946d335fce5edcc740d81390d30938ea5a8ab438
Shivam Singh <Shivam.singxh+git@gmail.com>
2025-05-13 21:45:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
 });
 
-server.listen(9000, () => console.log(`Server Started at PORT:9000`));
+server.listen(3000, () => console.log(`Server Started at PORT:3000`));
